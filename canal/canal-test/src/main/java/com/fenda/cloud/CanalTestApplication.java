7f8733d0024219e63e500f66e34233a0785e352e
fenda <qchen.sir@qq.com>
2022-12-29 10:52:00 +0800

    feat:canal-test

---
+++ canal/canal-test/src/main/java/com/fenda/cloud/CanalTestApplication.java
@@ -0,0 +1,13 @@
+package com.fenda.cloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CanalTestApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CanalTestApplication.class, args);
+    }
+
+}
